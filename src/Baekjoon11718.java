9451b0d4ffd9d467b51910097a07a0d91b696711
capdh <capdh@naver.com>
2017-03-10 16:36:21 +0900

    11718
    그대로 출력하기

---
+++ src/Baekjoon11718.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Baekjoon11718{
+	
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		String str;
+		while((str=sc.nextLine())!=null){
+			System.out.println(str);
+		}
+	}
+}
