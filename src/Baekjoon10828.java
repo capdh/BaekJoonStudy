e8dba4be07f9a1a62346a08208139f4a7405ab33
capdh <capdh@naver.com>
2017-03-14 15:30:14 +0900

    백준 10828(스택)
    백준 10828(스택)

---
+++ src/Baekjoon10828.java
@@ -0,0 +1,122 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon10828 {
+	public static void main(String[] args){
+		String inputString = null; // 입력받는 문자열
+		String[] splitString = new String[2]; // push 3 이런거를 push와 3으로 쪼개서 넣을 문자열 배열
+		int number = 0; // 몇번 실행할지
+		Stack myStack = new Stack(); //스택 생성
+		
+		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+		try{
+			number = Integer.parseInt(sc.readLine());
+		}
+		catch(IOException e){
+			e.getMessage();
+		}
+		
+		for(int i =0; i<number; i++){
+			try{
+				inputString = sc.readLine();
+				splitString = inputString.split(" ");
+			}
+			catch(IOException e){
+				e.getMessage();
+			}
+			
+			switch(splitString[0]){ 
+			case "push":
+				myStack.push(Integer.parseInt(splitString[1]));
+				break;
+			
+			case "pop":
+				myStack.pop();
+				break;
+				
+			case "size":
+				myStack.size();
+				break;
+				
+			case "empty":
+				myStack.empty();
+				break;
+				
+			case "top":
+				myStack.top();
+				break;
+			}
+			
+		}
+	}
+	
+		
+
+}
+
+class Stack{
+	int count;
+	Node headNode;
+	
+	public Stack(){
+		count = 0;
+		headNode = null;
+	}
+	
+	
+	void push(int data){
+		if(count==0){
+			Node newNode = new Node(data);
+			headNode = newNode;
+			count++;
+		}
+		
+		else{
+			Node newNode = new Node(data);
+			newNode.nextNode = headNode;
+			headNode = newNode;
+			count++;
+		}
+	}
+	
+	void pop(){
+		if(count > 0){
+			System.out.println(headNode.data);
+			headNode = headNode.nextNode;
+			count--;
+		}
+		else{
+			System.out.println(-1);
+		}
+		
+	}
+	
+	void size(){
+		System.out.println(count);
+	}
+	
+	void empty(){
+		if(count>0)
+			System.out.println(0);
+		else
+			System.out.println(1);
+	}
+	
+	void top(){
+		if(count>0)
+			System.out.println(headNode.data);
+		else
+			System.out.println(-1);
+	}
+	
+}
+
+class Node{
+	Node nextNode;
+	int data;
+	
+	public Node(int data){
+		this.data = data;
+	}
+}
--- src/Baekjoon11718.java
+++ src/Baekjoon11718.java
@@ -6,9 +6,32 @@ public class Baekjoon11718{
 	
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
+		
+	
 		String str;
-		while((str=sc.nextLine())!=null){
-			System.out.println(str);
-		}
+		String[] strSplit;
+		int result=0;
+		
+		//몇개의 숫자를 입력받을 건지
+		int count = sc.nextInt();
+		sc.nextLine();
+		
+		
+			str=sc.nextLine();
+			strSplit = str.split("");
+			
+			//
+			int[] numbers = new int[count];
+			
+			for(int j=0; j<count; j++){
+				numbers[j]=Integer.parseInt(strSplit[j]);
+			}
+			
+			for(int z=0; z<strSplit.length; z++){
+				result+=numbers[z];
+			}
+			System.out.println(result);
+
+		
 	}
 }
